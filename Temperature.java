02d2cf2b870678ed1b085db0d2866768f2a946a0
Deepthi_Muppala <Deepthi.Muppala@cerner.com>
2019-07-10 22:56:44 -0400

    Format changes

--- Temperature.java
+++ Temperature.java
@@ -1,27 +1,28 @@
 class Temperature {
-    private double temp;
-    private Temperature(double temp) {
-        this.temp = temp;
-    }
+  private double temp;
 
-    public static Temperature fromCentigrade(double centigrade) {
-        Temperature t1 = new Temperature(centigrade);
-        return t1;
-    }
+  private Temperature(double temp) {
+    this.temp = temp;
+  }
 
-    public static Temperature fromFahrenheit(double fahrenheit) {
-        double kelvin = (fahrenheit - 32) * (0.5556);
-        Temperature t2 = new Temperature(kelvin);
-        return t2;
-    }
+  public static Temperature fromCentigrade(double centigrade) {
+    Temperature t1 = new Temperature(centigrade);
+    return t1;
+  }
 
-    public double getCentigrade() {
-        return this.temp;
-    }
+  public static Temperature fromFahrenheit(double fahrenheit) {
+    double kelvin = (fahrenheit - 32) * (0.5556);
+    Temperature t2 = new Temperature(kelvin);
+    return t2;
+  }
 
-    public double getFahrenheit() {
-        double f = (this.temp * 9 / 5) + 32;
-        return f;
-    }
+  public double getCentigrade() {
+    return this.temp;
+  }
+
+  public double getFahrenheit() {
+    double f = (this.temp * 9 / 5) + 32;
+    return f;
+  }
 
 }
