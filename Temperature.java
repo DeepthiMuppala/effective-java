00fda4ffde26d10852741466675795c64d51cebf
Deepthi_Muppala <Deepthi.Muppala@cerner.com>
2019-07-10 22:52:18 -0400

    intial temperature class changes

---
+++ Temperature.java
@@ -0,0 +1,27 @@
+class Temperature {
+    private double temp;
+    private Temperature(double temp) {
+        this.temp = temp;
+    }
+
+    public static Temperature fromCentigrade(double centigrade) {
+        Temperature t1 = new Temperature(centigrade);
+        return t1;
+    }
+
+    public static Temperature fromFahrenheit(double fahrenheit) {
+        double kelvin = (fahrenheit - 32) * (0.5556);
+        Temperature t2 = new Temperature(kelvin);
+        return t2;
+    }
+
+    public double getCentigrade() {
+        return this.temp;
+    }
+
+    public double getFahrenheit() {
+        double f = (this.temp * 9 / 5) + 32;
+        return f;
+    }
+
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test {
+    public static void main(String[] args) {
+      double tempC = 38.9;
+      double tempF = 42.8;
+
+      Temperature t1 = Temperature.fromCentigrade(tempC);
+      Temperature t2 = Temperature.fromFahrenheit(tempF);
+
+      System.out.println("TempC in Fahrenheit is "+ t1.getFahrenheit());
+      System.out.println("TempF in Centigrade is "+ t2.getCentigrade());
+    }
+}
