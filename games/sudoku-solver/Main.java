e18c92a1d44dfe4af538dcdfef231d05214fe84d
Deepthi_Muppala <Deepthi.Muppala@cerner.com>
2019-08-19 22:03:59 -0400

    Addressed comments

--- games/sudoku-solver/Main.java
+++ games/sudoku-solver/Main.java
@@ -1,16 +1,8 @@
 class Main {
-  public void playSudoku(Sudoku sudoku) {
-    int[][] board = new int[9][9];
-    int randomRow = (int) Math.ceil(Math.random() * 9) - 1;
-    int randomCol = (int) Math.ceil(Math.random() * 9) - 1;
-    int randomVal = (int) Math.ceil(Math.random() * 9) - 1;
-    board[randomRow][randomCol] = randomVal;
-    sudoku.sudokuSolver(board);
-  }
-
+ 
   public static void main(String[] args) {
     Sudoku sudoku = new Sudoku();
-    Main m = new Main();
-    m.playSudoku(sudoku); 
+    int[][] board = sudoku.getSolvedBoard();
+    sudoku.printBoard(board);
   }
 }
--- games/sudoku-solver/Sudoku.java
+++ games/sudoku-solver/Sudoku.java
@@ -1,31 +1,39 @@
 import java.util.*;
 class Sudoku {
-  public void sudokuSolver(int[][] board) {
-    if(board.length == 0) return;
-    sudokuSolverHelper(board);
-    printBoard(board);
+  int[][] board = new int[9][9];
+
+  public Sudoku() {
+    int randomRow = (int) Math.ceil(Math.random() * 9) - 1;
+    int randomCol = (int) Math.ceil(Math.random() * 9) - 1;
+    int randomVal = (int) Math.ceil(Math.random() * 9);
+    board[randomRow][randomCol] = randomVal;
   }
 
-  public void printBoard(int[][] board) {
-    for(int i=0; i < board.length; i++) {
+  public int[][] getSolvedBoard() {
+    sudokuSolverHelper();
+    return board;
+  }
+
+  public void printBoard(int[][] solvedBoard) {
+    for(int i=0; i < solvedBoard.length; i++) {
       System.out.println();
-      for(int j=0; j< board[0].length; j++){
-        System.out.print(" " + board[i][j]);
+      for(int j=0; j< solvedBoard[0].length; j++){
+        System.out.print(" " + solvedBoard[i][j]);
       }
       System.out.println();
     }
   }
 
-  public boolean sudokuSolverHelper(int[][] board){
+  public boolean sudokuSolverHelper(){
     int row = board.length;
     int col = board[0].length;
     for (int i = 0; i < row; i++) {
       for (int j = 0; j < col; j++) {
         if(board[i][j] != 0) continue;
         for(int val = 1; val < 10; val++) {
-          if (isValid(i, j, board, val)) {
+          if (isValid(i, j, val)) {
             board[i][j] = val;
-            if(sudokuSolverHelper(board)) {
+            if(sudokuSolverHelper()) {
               return true;
             }
             else {
@@ -39,13 +47,14 @@ class Sudoku {
     return true;
   }
 
-  public boolean isValid(int i, int j, int[][] board, int val) {
+  public boolean isValid(int i, int j, int val) {
     for (int t = 0; t < board[0].length; t++) {
       if (board[i][t] != 0 && board[i][t] == val) {
         return false;
       } else if (board[t][j] != 0 && board[t][j] == val) {
         return false;
-      } else if (board[3 * (i / 3) + t / 3][3 * (j / 3) + t % 3] != 0 && board[3 * (i / 3) + t / 3][3 * (j / 3) + t % 3] == val)
+      } else if (board[3 * (i / 3) + t / 3][3 * (j / 3) + t % 3] != 0 && 
+                  board[3 * (i / 3) + t / 3][3 * (j / 3) + t % 3] == val)
         return false;
     }
     return true;
